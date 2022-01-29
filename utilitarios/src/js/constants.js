c95d2d6b38607ad8a2d28cf68095877f8db88af8
Raúl Armas <raularmasbx@gmail.com>
2022-01-29 13:53:17 -0500

    update

--- utilitarios/src/js/constants.js
+++ utilitarios/src/js/constants.js
@@ -4,3 +4,6 @@ export const URL_FISHES = "https://www.fishwatch.gov/api/species";
 
 export const URL_IMG_DOGS = "https://random.dog/woof.json";
 export const URL_IMG_CATS = "https://api.thecatapi.com/v1/images/search";
+
+//leerá el json de la dirección http 
+export const URL_JOKES = "https://api.chucknorris.io/jokes/random";
